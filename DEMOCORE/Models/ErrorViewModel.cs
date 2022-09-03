6b225c9a69831de5377262321065c13bfbcd4ae9
Kevin Crabtree <KEVCRABTREE325@STUDENT.CITRUSCOLLEGE.EDU>
2022-09-03 23:53:25 +0300

    Created First New REpo

---
+++ DEMOCORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DEMOCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
